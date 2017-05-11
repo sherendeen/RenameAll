733926c62c4129349de01ffdff0496c31ce30ac7
sherendeen <inivican@hotmail.com>
2017-05-10 21:27:10 -0400

    Made some changes to the manifest
    Notable changes: Changed Assembly Company.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RenameAll")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Renames all files within a specified directory")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Written by Seth G. R. Herendeen")]
 [assembly: AssemblyProduct("RenameAll")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
